5ced39205e2b1745139d0b26ecee249a2c9098ca
Brenton Cleeland <brenton@brntn.me>
2023-01-04 14:25:39 +1100

    feat: privacy policy

--- nicerun/urls.py
+++ nicerun/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import render
 
 from core.views import home, run
 
@@ -30,12 +31,18 @@ def security(request):
     )
 
 
+def privacy(request):
+    return render(request, 'privacy.html')
+
+
 def trigger_error(request):
     division_by_zero = 1 / 0
 
 
 urlpatterns = [
     path("", home),
+    path("privacy/", privacy),
+
     # .well-known
     path("robots.txt", robots),
     path(".well-known/security.txt", security),
